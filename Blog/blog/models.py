d2a8aa5c129cb32d843af8dff3c49554e2166f90
Tiago-Alcantara <Tiago.dpalcantara@gmail.com>
2023-09-27 15:33:44 -0300

    #3 Terceiro Checkpoint Primeiro model

--- Blog/blog/models.py
+++ Blog/blog/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
+
 # Create your models here.
 class Post(models.Model):
     autor = models.CharField(max_length = 255)
     titulo = models.CharField(max_length= 255)
-    sutitulo = models.CharField(max_length= 255)
-    counteudo = models.TextField()
-    date_publicacao = models.DateTimeField(default=datetime.now())
+    subtitulo = models.CharField(max_length= 255)
+    conteudo = models.TextField()
+    date_publicacao = models.DateTimeField(default=timezone.now())
+
+
+    def __str__(self):
+        return self.titulo
     
--- Blog/blog/views.py
+++ Blog/blog/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello, Word!!!</h1>")
+    
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html',context)
+
+
+def post_detail(request,post_id):
+    post = Post.objects.get(pk=post_id)
+    context = {
+        'post': post
+    }
+    print(post_id)
+
+    return render(request, 'blog/post_detail.html',context)
